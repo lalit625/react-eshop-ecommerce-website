86d839d7323d44b2e423402bcfb61f233a259cf7
lalit Bisht <lalit625@gmail.com>
2023-03-21 15:22:48 +0530

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+export const firebaseConfig = {
+    apiKey:process.env.REACT_APP_FB_API_KEY,
+    authDomain: "eshop-b1cde.firebaseapp.com",
+    projectId: "eshop-b1cde",
+    storageBucket: "eshop-b1cde.appspot.com",
+    messagingSenderId: "904507650743",
+    appId: "1:904507650743:web:1841de87a3abc617e1d332"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,95 @@
+import { useRef } from "react";
+import Card from "../../components/card/Card";
+import styles from "./Contact.module.scss";
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(form.current);
+
+    emailjs
+      .sendForm(
+        "service_9pv8n1u",
+        "template_fbjv4ci",
+        form.current,
+        "PZJVDLF9M_atOL3c7"
+      )
+      .then(
+        (result) => {
+          toast.success("Message sent successfully");
+        },
+        (error) => {
+          toast.error(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <section>
+      <div className={`container ${styles.contact}`}>
+        <h2>Contact Us</h2>
+        <div className={styles.section}>
+          <form ref={form} onSubmit={sendEmail}>
+            <Card cardClass={styles.card}>
+              <label>Name</label>
+              <input
+                type="text"
+                name="user_name"
+                placeholder="Full Name"
+                required
+              />
+              <label>Email</label>
+              <input
+                type="email"
+                name="user_email"
+                placeholder="Your active email"
+                required
+              />
+              <label>Subject</label>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                required
+              />
+              <label>Message</label>
+              <textarea name="message" cols="30" rows="10"></textarea>
+              <button className="--btn --btn-primary">Send Message</button>
+            </Card>
+          </form>
+
+          <div className={styles.details}>
+            <Card cardClass={styles.card2}>
+              <h3>Our Contact Information</h3>
+              <p>Fill the form or contact us via other channels listed below</p>
+              <div className={styles.icons}>
+                <span>
+                  <FaPhoneAlt />
+                  <p>+91 9871039560</p>
+                </span>
+                <span>
+                  <FaEnvelope />
+                  <p>Support@eshop.com</p>
+                </span>
+                <span>
+                  <GoLocation />
+                  <p>Delhi, India</p>
+                </span>
+               
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
